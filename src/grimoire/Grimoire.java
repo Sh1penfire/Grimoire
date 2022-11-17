f568d749ed357207ac3ceac533a6580af077a156
Sh1p*nfire <doominecraftcentral@gmail.com>
2022-11-17 17:41:41 +1100

    Removed unescecary clutter/tests

--- src/grimoire/Grimoire.java
+++ src/grimoire/Grimoire.java
@@ -9,26 +9,11 @@ import mindustry.game.EventType.*;
 import mindustry.mod.*;
 
 public class Grimoire extends Mod{
-
-    public Book book;
     public Grimoire(){
         Parsers.load();
 
-        // TODO: make a class to contain multiple pages, like a book.
-        Events.on(FileTreeInitEvent.class, e-> {
-        });
         Events.on(ClientLoadEvent.class, e -> {
-            loadBooks();
-            book.show();
+            new Book(new ZipFi(Vars.mods.getMod("grimoire").file).child("pages")).show();
         });
-    }
-
-    @Override
-    public void loadContent() {
-        //new Book(new ZipFi(Vars.mods.getMod("grimoire").file).child("pages")).show();
-    }
-
-    public void loadBooks(){
-        book = new Book(new ZipFi(Vars.mods.getMod("grimoire").file).child("pages"));
     }
 }
